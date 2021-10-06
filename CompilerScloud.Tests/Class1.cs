5b39fe63d9005573bbbff6c1aa50fa1178db515d
DeryaginAlex <92026368+DeryaginAlex@users.noreply.github.com>
2021-10-06 22:09:22 +0300

    tests

---
+++ CompilerScloud.Tests/Class1.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace CompilerScloud.Tests {    
+    [TestFixture]
+    public class Class1 {
+        [Test]
+        public void Test1() {
+            int i = 1;
+            i = i + 1;
+            Assert.AreEqual(1, i);
+        }
+    }
+}
